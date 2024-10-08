37b5cd0a002e24218d6769ac2cfcb4206896978e
Your Name <you@example.com>
2024-10-08 22:49:22 +0000

    get students by list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author ZaneleYenziwe
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
