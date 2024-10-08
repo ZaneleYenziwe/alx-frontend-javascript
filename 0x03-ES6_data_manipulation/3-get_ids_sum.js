f8c8a5d63d6ed2bd601085f3150b6eef08fcbe5c
Your Name <you@example.com>
2024-10-08 23:07:41 +0000

    get students by their id

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -3,6 +3,7 @@
  *
  * @param {Array<Object>} students - List of student objects.
  * @returns {Number} Sum of student IDs.
+ *@author ZaneleYenziwe <https://github.com/ZaneleYenziwe/alx-frontend-javascript.git>
  */
 export function getStudentIdsSum(students) {
   return students.reduce((sum, student) => sum + (link unavailable), 0);
