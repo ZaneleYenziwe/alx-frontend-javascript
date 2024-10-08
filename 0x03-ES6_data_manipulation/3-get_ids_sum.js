c051684f8b21997341835778514d1ae8cfebeeae
Your Name <you@example.com>
2024-10-08 22:33:14 +0000

    get students by ids sum

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+/**
+ * Returns the sum of all student IDs.
+ *
+ * @param {Array<Object>} students - List of student objects.
+ * @returns {Number} Sum of student IDs.
+ */
+export function getStudentIdsSum(students) {
+  return students.reduce((sum, student) => sum + (link unavailable), 0);
+}
+
