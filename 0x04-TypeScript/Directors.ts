d46b3d990f731aad21e0bf552dd229b654ee7a3b
Your Name <you@example.com>
2024-10-09 23:20:12 +0000

    interface that extends teacher

---
+++ 0x04-TypeScript/Directors.ts
@@ -0,0 +1,23 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
