6d3c3c2f47b6c03b3643a3513e6d9ebb9d48772c
Your Name <you@example.com>
2024-10-10 00:15:21 +0000

    listts subjects

---
+++ 0x04-TypeScript/task_4/Subject.ts
@@ -0,0 +1,12 @@
+namespace Subjects {
+  export class Subject {
+    private teacher: Subjects.Teacher;
+
+    public setTeacher(teacher: Subjects.Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
+
+
+
