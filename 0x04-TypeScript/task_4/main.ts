e167006f8cc06de0c745ac15d0398e56adb49c0f
Your Name <you@example.com>
2024-10-10 00:30:48 +0000

    exports constant

---
+++ 0x04-TypeScript/task_4/main.ts
@@ -0,0 +1,32 @@
+import { Cpp } from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
+import { Teacher } from './subjects/Teacher';
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+const cTeacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
+  experienceTeachingJava: 0,
+  experienceTeachingReact: 0,
+};
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
