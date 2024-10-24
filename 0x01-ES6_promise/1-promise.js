a047a1d698753210ca0530f0db376e61c2bbe5f3
ZaneleYenziwe <zaneleyenziwe19@gmail.com>
2024-10-24 15:56:02 +0000

    catch me if you can

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,8 @@
-import getFullResponseFromAPI from './1-promise';
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The fake API is not working currently'));
+  });
+}
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 1-main.js 
-Promise { { status: 200, body: 'Success' } }
-Promise {
-  <rejected> Error: The fake API is not working currently
-    ...
-    ...
+export default getFullResponseFromAPI;
